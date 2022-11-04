fc0c73ad61821196537c57674de8fee4074b7b31
brajesh-s <brajeshsekar@outlook.com>
2022-11-04 07:39:44 -0700

    Schema

---
+++ models/List.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose')
+
+const ListSchema = new mongoose.Schema(
+  {
+    title: { type: string, required: true, unique: true },
+    year: { type: number },
+    rated: { type: string },
+    runtime: { type: string },
+    genre: { type: string },
+    plot: { type: string },
+    language: { type: string },
+    poster: { type: string },
+    imdbRating: { type: string },
+    type: { type: Boolean, default: false },      
+  },
+  { timestamps: true }
+);
+
+module.export = mongoose.model('List', ListSchema);
