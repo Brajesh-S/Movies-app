6844a3a9366a058e5a92f30de85190b0d8ef8ca8
brajesh-s <brajeshsekar@outlook.com>
2022-11-18 03:31:08 -0800

    CRUD Operation

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const authRoute = require("./routes/auth");
+const userRoute = require('./routes/users')
 
 dotenv.config();
 
@@ -13,6 +14,7 @@ mongoose
 
 app.use(express.json());
 app.use('/api/auth', authRoute);
+app.use('/api/users', userRoute);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`${port}`));
