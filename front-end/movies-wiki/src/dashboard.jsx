8e42db5c0448ce1c222f9fa6eb9d7750876dccd0
Brajesh-S <Brajesh-S@users.noreply.github.com>
2023-11-28 13:56:28 +0530

    logout

--- front-end/movies-wiki/src/dashboard.jsx
+++ front-end/movies-wiki/src/dashboard.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { useAuth } from './authContext';
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
     const { logout } = useAuth();
+    const navigate = useNavigate();
 
     const handleLogout = () => {
       logout();
+      navigate('/');
     };
     return (
         <div>
