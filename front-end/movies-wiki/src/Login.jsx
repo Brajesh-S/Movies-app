b50baa48a908326dd5763d8756af674d07c1dd37
Brajesh-S <Brajesh-S@users.noreply.github.com>
2023-12-07 19:53:39 +0530

    register ui final

--- front-end/movies-wiki/src/Login.jsx
+++ front-end/movies-wiki/src/Login.jsx
@@ -1,118 +1,115 @@
 import React, { useState } from "react";
-import { useAuth } from './authContext'
+import { useAuth } from './authContext';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import './Login.css';
 
-
-
-
-
 export const Login = (props) => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('')
-    
-    const navigate = useNavigate();
-    const { setToken } = useAuth();
-    
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const { setToken } = useAuth();
 
-    const handleEmailChange = (e) => {
-        setEmail(e.target.value);
-    };
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
 
-    const handlePasswordChange = (e) => {
-        setPassword(e.target.value);
-    }; 
-    
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        
-     
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     const userData = {
-        email,
-        password,
-      };
-      await handleLogin(userData);
-    };  
+      email,
+      password,
+    };
+    await handleLogin(userData);
+  };
 
-    // Login.jsx
-
-const handleLogin = async (userData) => {
+  const handleLogin = async (userData) => {
     try {
-        console.log('Sending user data:', userData); 
-        const response = await axios.post('http://localhost:3000/api/auth/login', userData,
-            {
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            }
-        );
+      console.log('Sending user data:', userData);
+      const response = await axios.post(
+        'http://localhost:3000/api/auth/login',
+        userData,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
       console.log(response);
-         
-         
-    if (response.status === 200) {
+
+      if (response.status === 200) {
         const responseData = response.data;
-  
-        
-        if (responseData.error === false && responseData.data && responseData.data.accessToken) {
+
+        if (
+          responseData.error === false &&
+          responseData.data &&
+          responseData.data.accessToken
+        ) {
           console.log('Login successful.');
-  
-          setToken(responseData.data.accessToken);  
+          setToken(responseData.data.accessToken);
           navigate('/Dashboard');
         } else {
-          
           console.error('Login failed:', responseData.message);
         }
       } else {
-        
         console.error('Unexpected status code:', response.status);
       }
     } catch (error) {
-      
       console.error('Login failed. An error occurred:', error.message);
-    }   
+    }
   };
-  
-    return (
-      
-        <div className="login-page">
-         
-          <img className="fullscreen-bg-img" src={require("./homepage.jpg")} alt="" />
-        
-          <div className = "auth-form-container">
-          <div className="login-logo">
-            <img src={require("./Image4.png")} alt=""/>
-          </div>
-          <form className="login-form" onSubmit={handleSubmit}>
-              <label htmlFor="email" className="form-label">Email</label>
-              <input className="form-input"
-                  value={email}
-                  type="email"
-                  placeholder=""
-                  id="email"
-                  name="email"
-                  onChange={handleEmailChange}
-                  autoComplete="email"
-              />
-              <label htmlFor="password">Password</label>
-              <input className="form-input"
-                  value={password}
-                  type="password"
-                  placeholder=""
-                  id="password" 
-                  name="password" 
-                  onChange={handlePasswordChange}
-                  autoComplete="current-password"
-              />
-              <button type="submit" className="submit-button">Login</button>
-          </form>
-          
-          <button onClick={() => navigate('/register')} className="register-button">Don't have an account? Register here. </button>
-          </div>
-      </div>
-      
-    
-    );
-};
 
-  
+  return (
+    <div className="login-page">
+      <img
+        className="fullscreen-bg-img"
+        src={require("./homepage.jpg")}
+        alt=""
+      />
+      <div className="auth-form-container">
+        <div className="login-logo">
+          <img src={require("./Image4.png")} alt="" />
+        </div>
+        <form className="login-form" onSubmit={handleSubmit}>
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            className="form-input"
+            value={email}
+            type="email"
+            placeholder=""
+            id="email"
+            name="email"
+            onChange={handleEmailChange}
+            autoComplete="email"
+          />
+          <label htmlFor="password">Password</label>
+          <input
+            className="form-input"
+            value={password}
+            type="password"
+            placeholder=""
+            id="password"
+            name="password"
+            onChange={handlePasswordChange}
+            autoComplete="current-password"
+          />
+          <button type="submit" className="submit-button">
+            Login
+          </button>
+        </form>
+        <button
+          onClick={() => navigate('/register')}
+          className="register-button"
+        >
+          Don't have an account? Register here.
+        </button>
+      </div>
+    </div>
+  );
+};
--- front-end/movies-wiki/src/Register.jsx
+++ front-end/movies-wiki/src/Register.jsx
@@ -2,94 +2,102 @@ import React, { useState } from "react";
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import './register.css';
+
 export const Register = () => {
     const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('')
-    const [firstName, setFirstName] = useState('')
+    const [pass, setPass] = useState('');
+    const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
 
     const navigate = useNavigate();
-   
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        
+
         const userData = {
-            firstName:firstName,
-            lastName:lastName,
-            email:email,
-            password: pass, 
-          };   
-    
-          axios.post('http://localhost:3000/api/auth/register', userData)
-          .then(response => {
-            console.log(response.data);
-            
-          })
-          .catch(error => {
-            if (error.response) {
-              console.error('Response Data:', error.response.data);
-              console.error('Response Status:', error.response.status);
-            } else if (error.request) {
-              console.error('No response received:', error.request);
-            } else {
-              console.error('Error:', error.message);
-            }
-          });
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: pass,
+        };
+
+        axios.post('http://localhost:3000/api/auth/register', userData)
+            .then(response => {
+                console.log(response.data);
+            })
+            .catch(error => {
+                if (error.response) {
+                    console.error('Response Data:', error.response.data);
+                    console.error('Response Status:', error.response.status);
+                } else if (error.request) {
+                    console.error('No response received:', error.request);
+                } else {
+                    console.error('Error:', error.message);
+                }
+            });
     };
 
     return (
         <div className="login-page">
-          <div className="login-image"></div>
-        
-            <div className = "auth-form-container">
-                
+            <img className="fullscreen-bg-img" src={require("./homepage.jpg")} alt="" />
+
+            <div className="auth-form-container">
+                <div className="login-logo">
+                    <img src={require("./Image4.png")} alt="" />
+                </div>
+
                 <form className="register-form" onSubmit={handleSubmit}>
                     <label htmlFor="name">First name </label>
-                    <input className="form-input"
-                        value = {firstName}
-                        type = "text"
-                        placeholder = ""
-                        id = "name"
-                        name = "name"
+                    <input
+                        className="form-input"
+                        value={firstName}
+                        type="text"
+                        placeholder=""
+                        id="name"
+                        name="name"
                         onChange={(e) => setFirstName(e.target.value)}
                     />
+
                     <label htmlFor="name">Last name</label>
-                    <input className="form-input"
-                        value = {lastName}
-                        type = "text"
-                        placeholder = ""
-                        id = "name"
-                        name = "name"
+                    <input
+                        className="form-input"
+                        value={lastName}
+                        type="text"
+                        placeholder=""
+                        id="name"
+                        name="name"
                         onChange={(e) => setLastName(e.target.value)}
                     />
-                    
 
                     <label htmlFor="email">Email</label>
-                    <input className="form-input"
-                        value = {email}
-                        type = "email"
-                        placeholder = ""
-                        id = "email"
-                        name = "email"
+                    <input
+                        className="form-input"
+                        value={email}
+                        type="email"
+                        placeholder=""
+                        id="email"
+                        name="email"
                         onChange={(e) => setEmail(e.target.value)}
                     />
 
                     <label htmlFor="password">Password</label>
-                    <input className="form-input"
-                        value = {pass}
-                        type = "password"
-                        placeholder = ""
-                        id = "password" 
-                        name = "password"
-                        onChange={(e) => setPass(e.target.value)} 
+                    <input
+                        className="form-input"
+                        value={pass}
+                        type="password"
+                        placeholder=""
+                        id="password"
+                        name="password"
+                        onChange={(e) => setPass(e.target.value)}
                     />
-                    <button type="submit" className="submit-button" >Register</button>
+
+                    <button type="submit" className="submit-button">Register</button>
                 </form>
-                
-          
-                <button onClick={() => navigate('/')}className="login-here-button">Already have an account? Login here.</button>
-                </div>
-              </div>
-                )
-        }
+
+                <button onClick={() => navigate('/')} className="register-button">
+                    Already have an account? Login here.
+                </button>
+            </div>
+        </div>
+    );
+};
